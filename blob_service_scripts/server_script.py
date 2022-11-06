f107a47e205e6d57ea8494406fb70c6f31e809af
Luis Javier Horcajada Torres <72609987+luisjaa01@users.noreply.github.com>
2022-11-06 17:06:22 +0100

    Added server options
    Needs testing

--- blob_service_scripts/server_script.py
+++ blob_service_scripts/server_script.py
@@ -4,16 +4,108 @@
     Implementacion ejemplo de servidor y servicio REST
 '''
 
-from flask import Flask
+from flask import Flask, make_response
+import argparse
+import os
+from ipaddress import ip_address
 
 from blob_service.server import routeApp
 from blob_service.database import DataBase
+from blob_service.authService import AuthService, UserNotFound
 
 def main():
     '''Entry point'''
+    args = parse_args()
+    token = get_token(args)
+    port = get_port(args)
+    address = get_address(args)
+    database_path = get_database(args)
+    blob_storage = get_blob(args)
+    auth_server = get_authServer(args)
+    try:
+        user_token = auth_server.user_of_token(token)
+    except UserNotFound:
+        return make_response(f"User token not valid", 1)
+
+    
     app = Flask("database")
-    routeApp(app, DataBase("./dataBase.db"), 'http://127.0.0.1:5001/')
-    app.run(debug=True)
+    routeApp(app, DataBase(database_path), auth_server, blob_storage)
+    app.run(host= address, port=port,debug=True)
+
+def parse_args():
+    '''Argument Parser'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--admin", dest="admin_token", type=str, required=False, help="Admin token")
+    parser.add_argument("-p", "--port", dest="port", type=int, required=False, help="Port for server")
+    parser.add_argument("-l", "-listening", dest="address", type=ip_address, required=False, help="IP Address for server")
+    parser.add_argument("-d", "--db", dest="database_path", type=str, required=False, help="Path for database")
+    parser.add_argument("-s", "--storage", dest="blob_storage", type=str, required=False, help="Path for blob storage")
+    parser.add_argument("-u", "--url", dest="url", type=str, required=True, help="Authentication server url")
+
+    return parser.parse_args()
+
+def get_token(args):
+    '''Gets token from parser or generates default one'''
+    admin_token = args.admin_token
+    if admin_token is None:
+        admin_token = "token-admin"
+        print(f'Admin token generated: {admin_token}')
+        return admin_token
+    return admin_token
+
+def get_port(args):
+    '''Gets port from parser or generates default one'''
+    port = args.port
+    if port is None:
+        port = 3002
+        return port
+    if 1<= port <= 65535:
+        return port
+    
+    print("Invalid port number")
+    exit(1)
+
+def get_address(args):
+    '''Gets ip address from parser or generates default one'''
+    address = args.address
+    if address is None:
+        address = "0.0.0.0"
+        return address
+    return str(address)
+
+def get_database(args):
+    '''Gets database path from parser or generates default one'''
+    database = args.database_path
+    if database is None:
+        database = os.getcwd()
+        return database
+    else:
+        if os.path.exists(database) is True:
+            return database
+        else:
+            print("Path given for database doesn't exist")
+            exit(1)
+
+def get_blob(args):
+    '''Gets blob storage path from parser or generates default one'''
+    blob_path = args.blob_storage
+    if blob_path is None:
+        cwd = os.getcwd()
+        blob_path = os.path.join(cwd, 'storage')
+        if not os.path.exists(blob_path):
+            os.makedirs(blob_path)
+        return blob_path
+
+    else:
+        if os.path.exists(blob_path) is True:
+            return blob_path
+        else:
+            print("Path given for blob storage doesn't exist")
+            exit(1)
+
+def get_authServer(args):
+    '''Gets authentication server url'''
+    return AuthService(args.url)
 
 
 if __name__ == '__main__':
