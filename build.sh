9f7aef8d0000a2487bedf3c9e76440606fe212ce
algaru01 <agalvezruiz01@gmail.com>
2023-01-24 18:45:03 +0100

    Add generation of tar.gz files

--- build.sh
+++ build.sh
@@ -7,3 +7,7 @@ docker build --rm -f blob_service/Dockerfile --tag debian-blob ./blob_service
 docker build --rm -f auth_service/Dockerfile --tag debian-auth ./auth_service
 
 docker build --rm -f dir_service/Dockerfile --tag debian-dir ./dir_service
+
+docker save debian-blob:latest | gzip > blob_service.tar.gz
+docker save debian-auth:latest | gzip > auth_service.tar.gz
+docker save debian-dir:latest | gzip > dir_service.tar.gz
